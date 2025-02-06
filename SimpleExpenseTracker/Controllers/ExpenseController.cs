214383553f72763111af1016683202a22cea3729
charlesdominique <charlesdominiquebacanto@gmail.com>
2025-02-06 11:17:39 +0800

    Created the MVC components

---
+++ SimpleExpenseTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleExpenseTracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult IndexExpense()
+        {
+            return View();
+        }
+        
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleExpenseTracker/Models/ExpenseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleExpenseTracker.Models
+{
+    public class ExpenseModel
+    {
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
