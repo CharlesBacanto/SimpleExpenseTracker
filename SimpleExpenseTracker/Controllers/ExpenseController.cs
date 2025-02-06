9fcbf3809a72147854b4d07b64111d00a783a637
charlesdominique <charlesdominiquebacanto@gmail.com>
2025-02-06 11:27:09 +0800

    Create Form to handle inputs and Navigation for list and create delete form

--- SimpleExpenseTracker/Controllers/ExpenseController.cs
+++ SimpleExpenseTracker/Controllers/ExpenseController.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleExpenseTracker.Models;
 
 namespace SimpleExpenseTracker.Controllers
 {
     public class ExpenseController : Controller
     {
-        public IActionResult IndexExpense()
+        public IActionResult IndexExpense() // For the MasterList
         {
             return View();
         }
         
-        public IActionResult CreateEditExpense()
+        public IActionResult CreateEditExpense() // For Creating or Editing Expense Data
         {
             return View();
+        }
+        
+        public IActionResult CreateEditExpenseForm(ExpenseModel model) // For Handling the create edit form
+        {
+            return RedirectToAction("IndexExpense");
         }
     }
 }
